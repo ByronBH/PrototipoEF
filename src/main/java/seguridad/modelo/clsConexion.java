0d0e3fc59d7a7377665970098f081fe7701fc9c2
ByronBH <byronivan2405@hotmail.com>
2022-11-25 16:12:52 -0600

    se agrega el main

---
+++ src/main/java/seguridad/modelo/clsConexion.java
@@ -0,0 +1,47 @@
+package seguridad.modelo;
+
+import java.sql.*;
+
+public class clsConexion {
+
+
+
+
+    private static final String JDBC_URL = "jdbc:mysql://localhost/examenfinalbd?useSSL=false&serverTimezone=UTC";    
+     //private static final String JDBC_URL = "jdbc:sqlserver://localhost:1433;databaseName=proyectobd122022;"
+             // + "integratedSecurity=false;" + "encrypt=false;trustServerCertificate=false";
+          
+             
+
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASS = "";
+   
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+    }
+    
+    public static void close(ResultSet rs){
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(PreparedStatement stmt){
+        try {
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(Connection conn){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+           ex.printStackTrace(System.out);
+        }
+    }
+    
+}
